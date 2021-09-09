00a24a0a24e9f9a778f138bdab00d0bd76c6be1a
Elba Garza <elba@tamu.edu>
2021-09-08 19:19:07 -0500

    done with part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,43 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  tot = 0
+  arr.each{|a| tot+=a}
+  return tot
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.fetch(0) if arr.length < 2
+  return sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.length < 2
+  arr.each.with_index do |a, i|
+    arr.each.with_index do |b, j|
+      return true if ((a+b) == n and i != j)
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  # TODO YOUR CODE HERE
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # TODO YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # TODO YOUR CODE HERE
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+# TODO YOUR CODE HERE
 end
